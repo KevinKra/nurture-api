61a6deff003c50a06e3b556d57f0c42f1064b52e
Kevin Krato <kratok08@gmail.com>
2019-12-25 15:21:05 -0700

    Follow API design paradigm and adjust current convention

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::RegistrationsController < ApplicationController
+  def create
+    user = User.create!(
+      email: params["user"]["email"],
+      password: params["user"]["password"],
+      password_confirmation: params["user"]["password_confirmation"]
+    )
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: 200,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 500 }
+    end
+  end
+end
