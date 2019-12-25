a912acab48c66acfe1006e595cf3dbc5667cd65a
Kevin Krato <kratok08@gmail.com>
2019-12-25 11:39:46 -0700

    Create ExceptionHandler concern to handle 404 and 422 responses

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,14 @@
+module ExceptionHandler
+  # override nested failure responses with 404 | 422 response
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message }, :not_found)
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      json_response({ message: e.message }, :unprocessable_entity)
+    end
+  end
+end
