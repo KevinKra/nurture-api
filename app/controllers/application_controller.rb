d3cf4880588970249f23fe6ef20202c120ddb9a2
Kevin Krato <kratok08@gmail.com>
2019-12-25 12:21:43 -0700

    remove current_user method and replace with concern inclusions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
-
-  helper_method :current_user
-
-  def current_user
-    return false unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
-
+  include JsonResponse
+  include ExceptionHandler
+  include CurrentUser
 end
